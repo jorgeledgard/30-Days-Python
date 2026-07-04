fb5c478b98e7d0374c255d0d9512b80e97834116
Jorge Ledgard <ledgad.07@gmail.com>
2026-07-03 20:58:05 -0700

    Half of first day

---
+++ 01_Day_Introduction/day_1/helloworld.py
@@ -0,0 +1,8 @@
+# Exercise: Level 2 
+print(3 + 4) # addition
+print(3 - 4) # sustraction
+print(3 * 4) # multiplication
+print(3 % 4) # modules
+print(3 / 4) # division
+print(3 ** 4) # exponential
+print(3 // 4) # floor division operator
