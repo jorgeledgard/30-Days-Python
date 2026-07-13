5a93b56c1ad93eff9c0962cc47bfacff1e7b57f9
Jorge Ledgard <ledgad.07@gmail.com>
2026-07-12 19:15:30 -0700

    Almost complete Day 1: 1 excercise left

---
+++ 01_Day_Introduction/day_1/helloworld2.py
@@ -0,0 +1,25 @@
+# Exercise: Level 2 
+# 1 part: Do math
+print(3 + 4) # addition
+print(3 - 4) # sustraction
+print(3 * 4) # multiplication
+print(3 % 4) # modules
+print(3 / 4) # division
+print(3 ** 4) # exponential
+print(3 // 4) # floor division operator
+
+# 2 part: Print strings
+print("Jorge") # My name
+print("Ledgard") # Family name
+print("Mexico") # Country
+print("I am enjoying 30 days of python")
+
+# 3 part: Use type
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Jorge"))
+print(type("Ledgard"))
+print(type("Mexico"))
---
+++ 01_Day_Introduction/day_1/level_3.py
@@ -0,0 +1,12 @@
+#Level 3
+# 1 part: Examples of pyhton data types
+# I will use _type() so that when you run the command you can see exactly the data type
+print(type(19)) # My age
+print(type(49.51))
+print(type(1 + 2j))
+print(type("Hello, world"))
+print(type(True))
+print(type([49, 51, 1863]))
+print(type((3.14, 49.51, 1863.1864)))
+print(type({3.14, 49.51, 1863.1864}))
+print(type({'family':'country'}))
