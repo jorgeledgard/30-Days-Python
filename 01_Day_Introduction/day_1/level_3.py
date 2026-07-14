9caac49842eb1072abdec0bfba1fdaa480159949
Jorge Ledgard <ledgad.07@gmail.com>
2026-07-13 18:36:16 -0700

    Completed Day 1 and half of Day 2

--- 01_Day_Introduction/day_1/level_3.py
+++ 01_Day_Introduction/day_1/level_3.py
@@ -10,3 +10,13 @@ print(type([49, 51, 1863]))
 print(type((3.14, 49.51, 1863.1864)))
 print(type({3.14, 49.51, 1863.1864}))
 print(type({'family':'country'}))
+
+# 2 part: Find an euclidean distance between (2, 3) and (10, 8)
+# Variables
+x_1 = 2
+y_1 = 3
+x_2 = 10
+y_2 = 8
+# Do the formula of euclidean distance
+result = (((x_2 - x_1) ** 2) + ((y_2 - y_1) ** 2)) ** (1/2)
+print(result)
---
+++ 02_Day_Variables_builtin_functions/day_2/Variables2.py
@@ -0,0 +1,32 @@
+# Day 2: 30 Days of python programing
+# Level 1: Declaring variables
+first_name = "Jorge"
+last_name = "Ledgard"
+full_name = "Jorge Ledgard"
+country = "Mexico"
+city = "CDMX"
+age = 49
+year = 2026
+is_married = False
+is_true = True
+is_light_on = True
+x, y, z = "hola, mundo", 51, False
+
+# Level 2
+# 1 excersice
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(x))
+print(type(y))
+print(type(z))
+
+# 2 excersice
+print(len(first_name))
