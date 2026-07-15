bbf68bb9b497947abb53bfc02053f46d4ccddd2f
Jorge Ledgard <ledgad.07@gmail.com>
2026-07-15 13:42:18 -0700

    Complete Day 2

--- 02_Day_Variables_builtin_functions/day_2/Variables2.py
+++ 02_Day_Variables_builtin_functions/day_2/Variables2.py
@@ -13,7 +13,7 @@ is_light_on = True
 x, y, z = "hola, mundo", 51, False
 
 # Level 2
-# 1 excersice
+# Excersice 1
 print(type(first_name))
 print(type(last_name))
 print(type(full_name))
@@ -28,5 +28,67 @@ print(type(x))
 print(type(y))
 print(type(z))
 
-# 2 excersice
+# Exercise 2
 print(len(first_name))
+
+# Excercise 3 
+print(len(first_name))
+print(len(last_name))
+
+# Excercise 4
+num_one = 5
+num_two = 4
+
+# Excercise 5
+total = num_one + num_two
+print(total)
+
+# Excercise 6
+diff = num_one - num_two
+print(diff)
+
+# Excercise 7
+product = num_one * num_two
+print(product)
+
+# Excercise 8
+division = num_one / num_two
+print(division)
+
+# Excersice 9
+remainder = num_one % num_two
+print(remainder)
+
+# Excercise 10
+exp = num_one ** num_two
+print(exp)
+
+# Exercise 11 
+floor_division = num_one // num_two
+print(floor_division)
+
+# Excercise 12.1
+radius = 30
+pi = 3.1416
+_area_of_circle_ = pi * (radius ** 2)
+print(_area_of_circle_)
+
+# Excercise 12.2
+_circum_of_circle_ = 2 * radius + (2 * radius)
+print(_circum_of_circle_)
+
+# Excercise 12.3
+r = input("What is the radius?: ")
+area = pi * (float(r) ** 2)
+print("El area es: ", area)
+
+# Excersice 13
+_first_name_ = input("What is your name?: ")
+_last_name_ = input("What is oyur last name?: ")
+_country_ = input("Where are you from?: ")
+_age_ = input("How old are you?: ")
+print(_first_name_)
+print(_last_name_)
+print(_country_)
+print(_age_)
+
