b7504893a7f8f295f909e7376ca654aaa0647de9
Jorge Ledgard <ledgad.07@gmail.com>
2026-07-26 21:40:14 -0700

    Half 4 day

---
+++ 04_Day_Strings/day_4/day4_short_excercises.py
@@ -0,0 +1,61 @@
+# Excercise 1
+a = "Thirty"
+b = "Days"
+c = "Of"
+d = "Python"
+print(f"{a} {b} {c} {d}")
+
+# Excercise 2
+e = "Coding"
+f = "For"
+g = "All"
+print(f"{e} {f} {g}")
+
+# Excercise 3
+company = "Coding For All"
+
+# Excercise 4
+print(company)
+
+# Excercise 5
+print(len(company))
+
+# Excercise 6
+print(company.upper())
+
+# Excercise 7
+print(company.lower())
+
+# Excercise 8
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+
+# Excercise 9
+print(company[0:6])
+
+# Escercise 10
+print(company.startswith("Coding"))
+
+# Excercise 11
+print(company.replace("Coding", "Python"))
+
+# Excercise 12
+comp = "Python for Everyone"
+print(comp.replace("Everyone", "All"))
+
+# Excercise 13
+print(company.split(" "))
+
+# Excercise 14
+apps = "Facebook Google Microsoft Apple IBM Oracle Amazon"
+print(apps.split())
+
+# Excercise 15
+print(company[0]) # C
+
+# Excercise 16
+print(company.rindex("l")) # 13
+
+# Excercise 17
+print(company[10])
